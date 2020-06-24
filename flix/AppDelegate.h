af7c679e5740ba98091b4ee9527a0340f3b4413a
Olalekan Abdulrazaq Adisa <adisaolalekan@fb.com>
2020-06-24 13:51:48 -0500

    Initial Commit

---
+++ flix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  flix
+//
+//  Created by Olalekan Abdurazaq Adisa on 6/24/20.
+//  Copyright © 2020 Facebook University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ flix/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  flix
+//
+//  Created by Olalekan Abdurazaq Adisa on 6/24/20.
+//  Copyright © 2020 Facebook University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
